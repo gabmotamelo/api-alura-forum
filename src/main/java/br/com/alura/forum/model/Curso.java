7853267988819d3add96491a1f0a9406227a9dff
Gabriel Mota <gabrielmota6@hotmail.com>
2022-09-13 16:49:49 -0300

    Utilizando o repository

--- src/main/java/br/com/alura/forum/model/Curso.java
+++ src/main/java/br/com/alura/forum/model/Curso.java
@@ -14,13 +14,6 @@ public class Curso {
 	private String nome;
 	private String categoria;
 
-	public Curso(String nome, String categoria) {
-		this.nome = nome;
-		this.categoria = categoria;
-	}
-
-	public Curso() {}
-
 	@Override
 	public int hashCode() {
 		final int prime = 31;
