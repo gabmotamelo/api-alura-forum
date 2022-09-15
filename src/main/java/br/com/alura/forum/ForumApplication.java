18a1e5f87b402955cce26796c631f9bab88d4b10
Gabriel Mota <gabrielmota6@hotmail.com>
2022-09-14 21:04:45 -0300

    Habilitando o web security no projeto

--- src/main/java/br/com/alura/forum/ForumApplication.java
+++ src/main/java/br/com/alura/forum/ForumApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport
 @EnableCaching
+@EnableWebSecurity
 public class ForumApplication {
 
 	public static void main(String[] args) {
