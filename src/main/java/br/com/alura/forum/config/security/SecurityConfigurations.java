b1e4c780e99e9dd68ac329e4c03dea09808fc954
Gabriel Mota <gabrielmota6@hotmail.com>
2022-10-04 20:40:05 -0300

    Adicionando os profiles de dev e prod

--- src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -4,10 +4,12 @@ import br.com.alura.forum.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -15,8 +17,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-
+@EnableWebSecurity
 @Configuration
+@Profile("prod")
 public class SecurityConfigurations {
 
     @Autowired
@@ -45,7 +48,7 @@ public class SecurityConfigurations {
         .antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
         .antMatchers(HttpMethod.POST, "/auth").permitAll()
         .antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
-        .antMatchers(HttpMethod.GET, "/").permitAll()
+        .antMatchers(HttpMethod.DELETE, "/topicos/*").hasRole("MODERADOR")
         .antMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html", "/**/api-docs").permitAll()
         .antMatchers("**/favicon.ico", "/css/**","/images/**", "/js/**", "/webjars/**").permitAll()
         .anyRequest().authenticated()
--- src/main/java/br/com/alura/forum/config/swagger/SwaggerConfigurations.java
+++ src/main/java/br/com/alura/forum/config/swagger/SwaggerConfigurations.java
@@ -1,67 +1,70 @@
-package br.com.alura.forum.config.swagger;
-
-
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import io.swagger.v3.oas.annotations.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class SwaggerConfigurations {
-
-//    @Bean
-//    public GroupedOpenApi forumApi() {
-//        return GroupedOpenApi.builder()
-//                .group("forumApi")
-//                .pathsToMatch("/**")
-//                .build();
-//    }
+//package br.com.alura.forum.config.swagger;
 //
-//    @Bean
-//    public OpenAPI forumAluraOpenAPI() {
-//        return new OpenAPI()
-//                .info(new Info().title("Forum API")
-//                .description("Projeto de Documentacao de API da Alura")
-//                .version("v0.0.1")
-//                .license(new License().name("Apache 2.0").url("http://springdoc.org")))
-//                .components(new Components().addSecuritySchemes("bearer-key", new SecurityScheme().type(SecurityScheme.Type.HTTP)
-//                    .scheme("bearer")
-//                    .bearerFormat("JWT")))
-//                .externalDocs(new ExternalDocumentation()
-//                    .description("SpringShop Wiki Documentation")
-//                    .url("https://springshop.wiki.github.org/docs"))
-//                .components(new Components()
-//                        .addSecuritySchemes("bearer-key", new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").in(SecurityScheme.In.HEADER).bearerFormat("JWT")));
-//    }
-
-    @Value("${info.app.version}")
-    private String version;
-
-    @Value("${springdoc.info.description}")
-    private String description;
-
-    @Value("${springdoc.info.title}")
-    private String title;
-
-    @Bean
-    public OpenAPI infoApi() {
-
-        Info info = new Info();
-        info.setTitle(title);
-
-        SecurityRequirement securityItem = new SecurityRequirement();
-        securityItem.addList("Authorization");
-
-        Components components = new Components();
-        components.addSecuritySchemes("Authorization", new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT"));
-
-        return new OpenAPI().components(components)
-                .addSecurityItem(securityItem)
-                .info(info.description(description).version(version));
-    }
-
-}
+//
+//import io.swagger.v3.oas.annotations.ExternalDocumentation;
+//import io.swagger.v3.oas.annotations.info.Info;
+//import io.swagger.v3.oas.annotations.info.License;
+//import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+//import io.swagger.v3.oas.annotations.security.SecurityScheme;
+////import io.swagger.v3.oas.annotations.Components;
+//import io.swagger.v3.oas.models.OpenAPI;
+//import org.springdoc.core.GroupedOpenApi;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class SwaggerConfigurations {
+//
+////    @Bean
+////    public GroupedOpenApi forumApi() {
+////        return GroupedOpenApi.builder()
+////                .group("forumApi")
+////                .pathsToMatch("/**")
+////                .build();
+////    }
+////
+////    @Bean
+////    public OpenAPI forumAluraOpenAPI() {
+////        return new OpenAPI()
+////                .info(new Info().title("Forum API")
+////                .description("Projeto de Documentacao de API da Alura")
+////                .version("v0.0.1")
+////                .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+////                .components(new Components().addSecuritySchemes("bearer-key", new SecurityScheme().type(SecurityScheme.Type.HTTP)
+////                    .scheme("bearer")
+////                    .bearerFormat("JWT")))
+////                .externalDocs(new ExternalDocumentation()
+////                    .description("SpringShop Wiki Documentation")
+////                    .url("https://springshop.wiki.github.org/docs"))
+////                .components(new Components()
+////                        .addSecuritySchemes("bearer-key", new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").in(SecurityScheme.In.HEADER).bearerFormat("JWT")));
+////    }
+//
+//    @Value("${info.app.version}")
+//    private String version;
+//
+//    @Value("${springdoc.info.description}")
+//    private String description;
+//
+//    @Value("${springdoc.info.title}")
+//    private String title;
+//
+////    @Bean
+////    public OpenAPI infoApi() {
+////
+////        Info info = new Info();
+////        info.setTitle(title);
+////
+////        SecurityRequirement securityItem = new SecurityRequirement();
+////        securityItem.addList("Authorization");
+////
+////        Components components = new Components();
+////        components.addSecuritySchemes("Authorization", new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT"));
+////
+////        return new OpenAPI().components(components)
+////                .addSecurityItem(securityItem)
+////                .info(info.description(description).version(version));
+////    }
+//
+//}
